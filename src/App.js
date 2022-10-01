02f2ba24fa6b58e60fd9c21301fcc24f2147974a
Bruno Ceccolini <34370581+BrunoCecco@users.noreply.github.com>
2022-10-01 10:32:18 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-slate-800 p-20">
+      <div className='text-white font-extrabold text-6xl'>BRUNO CECCOLINI</div>
+      <div>
+      Interests 
+- crypto 
+- Sports
+Creative css
+University
+Projects 
+-realms
+-DAO chat 
+-Xchanted 
+-Solswap 
+-Motive 
+-Mactutor 
+      </div>
+    </div>
+  );
+}
+
+export default App;
